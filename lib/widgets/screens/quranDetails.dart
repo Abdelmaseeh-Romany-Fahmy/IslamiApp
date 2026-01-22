b6ab720e4dc2675c32c4b83f156c7e296f363495
Abdelmaseeh-Romany-Fahmy <apdelmaseehromany@gmail.com>
2026-01-22 23:19:38 +0200

    % quranDetails.dart %quran +QuranDetailFaile

--- lib/widgets/screens/quranDetails.dart
+++ lib/widgets/screens/quranDetails.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../ui/assets_image.dart';
 import '../tabs/quran.dart';
 
-class Qurandetails extends StatelessWidget {
+class Qurandetails extends StatefulWidget {
   static String routName ="Qurandetails";
-  const Qurandetails({super.key});
+
+   Qurandetails({super.key});
 
   @override
+  State<Qurandetails> createState() => _QurandetailsState();
+}
+
+class _QurandetailsState extends State<Qurandetails> {
+ ReadSouraDetailsFile(int index,)async{
+    String data =await rootBundle.loadString("assets/souraDetailsfFile/${index+1}.txt");
+    // verses=  data.trim().split("\n");
+List<String> lines= data.trim().split("\n");
+verses =List.generate(lines.length, (i) => " (${i + 1}) ${lines[i]}",);
+
+     setState(() {
+
+     });
+
+
+
+  }
+
+  List<String> verses =[];
+
+  @override
+
   Widget build(BuildContext context) {
     ModelQuran modelQuran= ModalRoute.of(context)!.settings.arguments as ModelQuran ;
-    return Container(
-    
+     if (verses.isEmpty) ReadSouraDetailsFile(modelQuran.index,);
+    return
+     Container(
+
         decoration: BoxDecoration(
-        
+
             image: DecorationImage(image: AssetImage(Assets_Image.default_bg_Light),fit: BoxFit.cover)
         ),
         child: Scaffold(
@@ -24,17 +50,26 @@ class Qurandetails extends StatelessWidget {
         title: Text("إسلامي"),
     ),
         body: Container(
-          
+
           margin: EdgeInsets.all(20),
-          height:MediaQuery.of(context).size.height*0.652 ,
-          width: MediaQuery.of(context).size.width*3.54,
 
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(25) ,
               color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.8)),
           child: Column(
             children: [
-              Text(modelQuran.name,style: Theme.of(context).textTheme.titleMedium,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(modelQuran.name,style: Theme.of(context).textTheme.titleMedium,textAlign: TextAlign.center,),
+                  ),
+                  SizedBox(width: 40,),
+                  Icon(Icons.play_circle,size: 30,)
+                ],
+              ),
               Divider(
                 thickness: 2,
                 endIndent: 42.5,
@@ -42,7 +77,30 @@ class Qurandetails extends StatelessWidget {
 
 
 
+              ),
+              verses.isEmpty?Center(
+                  heightFactor: 15,
+                  child:
+
+              CircularProgressIndicator()):
+              Expanded(
+
+
+
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                    itemCount: verses.length,
+                    itemBuilder: (context, index) {
+                  return  Container(
+                      child: Center(
+
+                          child: Text(verses[index],style: Theme.of(context).textTheme.titleMedium,)),
+                    );
+                  },),
+                ),
               )
+
             ],
 
           ),
@@ -54,5 +112,7 @@ class Qurandetails extends StatelessWidget {
 
 
     );
+
   }
+
 }
--- lib/widgets/tabs/quran.dart
+++ lib/widgets/tabs/quran.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islami2026/ui/assets_image.dart';
 import 'package:islami2026/widgets/screens/quranDetails.dart';
 
-class quranTab extends StatelessWidget {
+class quranTab extends StatefulWidget {
+
+   quranTab({super.key});
+
+  @override
+  State<quranTab> createState() => _quranTabState();
+}
+
+class _quranTabState extends State<quranTab> {
 List<String>  surasName=["الفاتحه","البقرة","آل عمران","النساء","المائدة","الأنعام","الأعراف","الأنفال","التوبة","يونس","هود"
   ,"يوسف","الرعد","إبراهيم","الحجر","النحل","الإسراء","الكهف","مريم","طه","الأنبياء","الحج","المؤمنون"
   ,"النّور","الفرقان","الشعراء","النّمل","القصص","العنكبوت","الرّوم","لقمان","السجدة","الأحزاب","سبأ"
@@ -13,33 +22,88 @@ List<String>  surasName=["الفاتحه","البقرة","آل عمران","ال
   ,"المطفّفين","الإنشقاق","البروج","الطارق","الأعلى","الغاشية","الفجر","البلد","الشمس","الليل","الضحى","الشرح"
   ,"التين","العلق","القدر","البينة","الزلزلة","العاديات","القارعة","التكاثر","العصر",
   "الهمزة","الفيل","قريش","الماعون","الكوثر","الكافرون","النصر","المسد","الإخلاص","الفلق","الناس"];
-   quranTab({super.key});
+
+List<int>  countVerses =[];
+
+readCountVerses()async{
+  List<int>  _countVerses =[];
+for(var i =0 ; i< surasName.length;i++){
+  String data =await rootBundle.loadString("assets/souraDetailsfFile/${i+1}.txt");
+  List<String> verses=  data.trim().split("\n");
+  verses.removeWhere(
+        (element) => element.trim().isEmpty,
+  );
+  _countVerses.add(verses.length);
+  setState(() {
+
+  });
+
+}
+
+countVerses =_countVerses;
+
+
+
+}
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(Assets_Image.quran_logo,height: MediaQuery.of(context).size.height*0.25),
-        Expanded(
-          child: ListView.separated(
-            itemCount: surasName.length,
-              itemBuilder: (context, index) => Center(
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).pushNamed(Qurandetails.routName,arguments: ModelQuran(surasName[index], index));
-                  },
+  if(countVerses.isEmpty)readCountVerses();
+    return Stack(
+      children:[
+        Column(
+        children: [
+          Image.asset(Assets_Image.quran_logo,height: MediaQuery.of(context).size.height*0.25),
+          Divider(
+            thickness: 2,
+          ),
+          Row(
+            children: [
+              Expanded(child: Text("عدد الايات  ",style: Theme.of(context).textTheme.titleMedium,textAlign: TextAlign.center,)),
 
-                  child: Text(surasName[index],style: Theme.of(context).textTheme.titleMedium,
+              Expanded(child: Text("اسم الصوره ",style: Theme.of(context).textTheme.titleMedium,textAlign: TextAlign.center)),
+
+            ],
+          ),
+          Divider(
+            thickness: 2,
+          ),
+          Expanded(
+              child:countVerses.isEmpty?Center(child: CircularProgressIndicator()): ListView.builder(
+                itemCount: surasName.length,
+                itemBuilder: (context, index) => Center(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).pushNamed(Qurandetails.routName,arguments: ModelQuran(surasName[index], index));
+                    },
+
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(countVerses[index].toString(),style: Theme.of(context).textTheme.titleMedium,textAlign: TextAlign.center,),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(surasName[index],style: Theme.of(context).textTheme.titleMedium,textAlign: TextAlign.center,),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ), separatorBuilder: (BuildContext context, int index) {
-              return Divider(
-                thickness:2,
-              );
-          },
-        ))
+              ))
 
-      ],
+        ],
+      ),
+        Center(child: VerticalDivider(
+          thickness: 2,
+          indent: MediaQuery.of(context).size.height*0.26,
+        ))
+      ]
     );
   }
 }
@@ -48,5 +112,4 @@ class ModelQuran{
  final int index;
 
   ModelQuran(this.name, this.index);
-
 }
